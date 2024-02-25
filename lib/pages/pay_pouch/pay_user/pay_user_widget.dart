871b680b673f7fd599ba2841f4a6d0511eabb8b3
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-25 18:25:13 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/pay_pouch/pay_user/pay_user_widget.dart
+++ lib/pages/pay_pouch/pay_user/pay_user_widget.dart
@@ -1,7 +1,6 @@
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'pay_user_model.dart';
@@ -23,11 +22,6 @@ class _PayUserWidgetState extends State<PayUserWidget> {
   void initState() {
     super.initState();
     _model = createModel(context, () => PayUserModel());
-
-    // On page load action.
-    SchedulerBinding.instance.addPostFrameCallback((_) async {
-      context.safePop();
-    });
   }
 
   @override
