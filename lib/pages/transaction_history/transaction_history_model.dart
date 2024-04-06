6d38eb138f3c507a218918291a21f6c6f3ce2985
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-04-06 11:55:06 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/transaction_history/transaction_history_model.dart
+++ lib/pages/transaction_history/transaction_history_model.dart
@@ -3,13 +3,17 @@ import '/flutter_flow/flutter_flow_util.dart';
 import 'dart:async';
 import 'transaction_history_widget.dart' show TransactionHistoryWidget;
 import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 class TransactionHistoryModel
     extends FlutterFlowModel<TransactionHistoryWidget> {
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
-  Completer<ApiCallResponse>? apiRequestCompleter;
+  // State field(s) for ListView widget.
+
+  PagingController<ApiPagingParams, dynamic>? listViewPagingController;
+  Function(ApiPagingParams nextPageMarker)? listViewApiCall;
 
   @override
   void initState(BuildContext context) {}
@@ -17,10 +21,50 @@ class TransactionHistoryModel
   @override
   void dispose() {
     unfocusNode.dispose();
+    listViewPagingController?.dispose();
   }
 
   /// Additional helper methods.
-  Future waitForApiRequestCompleted({
+  PagingController<ApiPagingParams, dynamic> setListViewController(
+    Function(ApiPagingParams) apiCall,
+  ) {
+    listViewApiCall = apiCall;
+    return listViewPagingController ??= _createListViewController(apiCall);
+  }
+
+  PagingController<ApiPagingParams, dynamic> _createListViewController(
+    Function(ApiPagingParams) query,
+  ) {
+    final controller = PagingController<ApiPagingParams, dynamic>(
+      firstPageKey: ApiPagingParams(
+        nextPageNumber: 0,
+        numItems: 0,
+        lastResponse: null,
+      ),
+    );
+    return controller
+      ..addPageRequestListener(listViewGetTransactionHistoryPage);
+  }
+
+  void listViewGetTransactionHistoryPage(ApiPagingParams nextPageMarker) =>
+      listViewApiCall!(nextPageMarker)
+          .then((listViewGetTransactionHistoryResponse) {
+        final pageItems = (listViewGetTransactionHistoryResponse.jsonBody ?? [])
+            .toList() as List;
+        final newNumItems = nextPageMarker.numItems + pageItems.length;
+        listViewPagingController?.appendPage(
+          pageItems,
+          (pageItems.isNotEmpty)
+              ? ApiPagingParams(
+                  nextPageNumber: nextPageMarker.nextPageNumber + 1,
+                  numItems: newNumItems,
+                  lastResponse: listViewGetTransactionHistoryResponse,
+                )
+              : null,
+        );
+      });
+
+  Future waitForOnePageForListView({
     double minWait = 0,
     double maxWait = double.infinity,
   }) async {
@@ -28,7 +72,8 @@ class TransactionHistoryModel
     while (true) {
       await Future.delayed(const Duration(milliseconds: 50));
       final timeElapsed = stopwatch.elapsedMilliseconds;
-      final requestComplete = apiRequestCompleter?.isCompleted ?? false;
+      final requestComplete =
+          (listViewPagingController?.nextPageKey?.nextPageNumber ?? 0) > 0;
       if (timeElapsed > maxWait || (requestComplete && timeElapsed > minWait)) {
         break;
       }
